552191bc52ac4d8f30b580ee5581186288e117dd
Denis Muradov <denis.muradov@yandex.ru>
2022-09-06 19:22:14 +0300

    Создать контекст юзера, использовать контекст в компоненте Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from './ImagePopup';
+import { api } from './utils/api';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function App() {
 
@@ -13,6 +15,13 @@ function App() {
   const [isConfirmPopupOpen, setConfirmPopupOpen] = useState(false)
   const [selectedCard, setSelectedCard] = useState({})
   const [isImagePopupOpened, setImagePopupOpened] = useState(false)
+  const [currentUser, setCurrentUser] = useState({})
+  
+  useEffect(() => {
+    api.getUserInfo()
+      .then(user => setCurrentUser(user))
+      .catch(err => console.log(err))
+  }, [])
 
   function handleCardClick(card) {
     setSelectedCard(card)
@@ -42,122 +51,124 @@ function App() {
   }
   
   return (
-    <div className="page__container">
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page__container">
 
-      <Header />
+        <Header />
 
-      <Main
-        onEditAvatar={handleEditAvatarClick}
-        onAddPlace={handleAddPlaceClick}
-        onEditProfile={handleEditProfileClick}
-        onDeleteBtnClick={handleDeleteBtnClick}
-        onCardClick={handleCardClick}
-      />
+        <Main
+          onEditAvatar={handleEditAvatarClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditProfile={handleEditProfileClick}
+          onDeleteBtnClick={handleDeleteBtnClick}
+          onCardClick={handleCardClick}
+        />
 
-      <Footer />
+        <Footer />
 
-      <ImagePopup
-        card={selectedCard}
-        isOpen={isImagePopupOpened}
-        onClose={closeAllPopups}
-      />
+        <ImagePopup
+          card={selectedCard}
+          isOpen={isImagePopupOpened}
+          onClose={closeAllPopups}
+        />
 
-      <PopupWithForm
-        name="avatar"
-        title="Обновить аватар"
-        isOpen={isEditAvatarPopupOpen}
-        onClose={closeAllPopups}
-      >
-        <fieldset className="form__inputs">
-          <label className="form__field">
-            <input 
-              id="avatar-input"
-              name="avatar-link"
-              className="form__item form__item_el_avatar"
-              type="url"
-              placeholder="https://somewebsite.com/someimage.jpg"
-              required
-            />
-            <span className="avatar-input-error form__item-error"></span>
-          </label>
-        </fieldset>
-      </PopupWithForm>
-      <PopupWithForm
-        name="user"
-        title="Редактировать профиль"
-        isOpen={isEditProfilePopupOpen}
-        onClose={closeAllPopups}
-      >
-        <fieldset className="form__inputs">
-          <label className="form__field">
-            <input
-              id="user-name-input"
-              className="form__item form__item_el_user-name" 
-              name="user-name"
-              type="text" 
-              placeholder="Имя"
-              minLength="2"
-              maxLength="40"
-              required
-            />
-            <span className="user-name-input-error form__item-error"></span>
-          </label>
-          <label className="form__field">
-            <input
-              id="activity-input"
-              className="form__item form__item_el_user-activity" 
-              name="user-activity"
-              type="text" 
-              placeholder="О себе"
-              minLength="2"
-              maxLength="200"
-              required
-            />
-            <span className="activity-input-error form__item-error"></span>
-          </label>
-        </fieldset>
-      </PopupWithForm>
-      <PopupWithForm
-        name="card"
-        title="Новое место"
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-      >
-        <fieldset className="form__inputs">
-          <label className="form__field">
-            <input
-              id="card-name-input"
-              className="form__item form__item_el_card-name" 
-              name="card-name"
-              type="text" 
-              placeholder="Название"
-              minLength="2"
-              maxLength="30"
-              required
-            />
-            <span className="card-name-input-error form__item-error"></span>
-          </label>
-          <label className="form__field">
-            <input
-              id="link-input"
-              className="form__item form__item_el_card-link" 
-              name="card-link"
-              type="url" 
-              placeholder="Ссылка на картинку"
-              required
-            />
-            <span className="link-input-error form__item-error"></span>
-          </label>
-        </fieldset>                                    
-      </PopupWithForm>
-      <PopupWithForm
-        name="confirm"
-        title="Вы уверены?"
-        isOpen={isConfirmPopupOpen}
-        onClose={closeAllPopups}
-      />
+        <PopupWithForm
+          name="avatar"
+          title="Обновить аватар"
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+        >
+          <fieldset className="form__inputs">
+            <label className="form__field">
+              <input 
+                id="avatar-input"
+                name="avatar-link"
+                className="form__item form__item_el_avatar"
+                type="url"
+                placeholder="https://somewebsite.com/someimage.jpg"
+                required
+              />
+              <span className="avatar-input-error form__item-error"></span>
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithForm
+          name="user"
+          title="Редактировать профиль"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <fieldset className="form__inputs">
+            <label className="form__field">
+              <input
+                id="user-name-input"
+                className="form__item form__item_el_user-name" 
+                name="user-name"
+                type="text" 
+                placeholder="Имя"
+                minLength="2"
+                maxLength="40"
+                required
+              />
+              <span className="user-name-input-error form__item-error"></span>
+            </label>
+            <label className="form__field">
+              <input
+                id="activity-input"
+                className="form__item form__item_el_user-activity" 
+                name="user-activity"
+                type="text" 
+                placeholder="О себе"
+                minLength="2"
+                maxLength="200"
+                required
+              />
+              <span className="activity-input-error form__item-error"></span>
+            </label>
+          </fieldset>
+        </PopupWithForm>
+        <PopupWithForm
+          name="card"
+          title="Новое место"
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <fieldset className="form__inputs">
+            <label className="form__field">
+              <input
+                id="card-name-input"
+                className="form__item form__item_el_card-name" 
+                name="card-name"
+                type="text" 
+                placeholder="Название"
+                minLength="2"
+                maxLength="30"
+                required
+              />
+              <span className="card-name-input-error form__item-error"></span>
+            </label>
+            <label className="form__field">
+              <input
+                id="link-input"
+                className="form__item form__item_el_card-link" 
+                name="card-link"
+                type="url" 
+                placeholder="Ссылка на картинку"
+                required
+              />
+              <span className="link-input-error form__item-error"></span>
+            </label>
+          </fieldset>                                    
+        </PopupWithForm>
+        <PopupWithForm
+          name="confirm"
+          title="Вы уверены?"
+          isOpen={isConfirmPopupOpen}
+          onClose={closeAllPopups}
+        />
 
-    </div>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { api } from './utils/api';
 import Card from './Card';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Main(props) {
+	const user = useContext(CurrentUserContext)
 
-	const [userName, setUserName] = useState(null)
-	const [userAvatar, setUserAvatar] = useState(null)
-	const [userDescription, setUserDescription] = useState(null)
 	const [cards, setCards] = useState([])
-	
-	useEffect(() => {
-		api.getUserInfo()
-			.then((user) => {
-				setUserName(user.name);
-				setUserAvatar(user.avatar);
-				setUserDescription(user.about);
-			})
-			.catch(err => console.log(err))
-		}, []
-	)
 	
 	useEffect(() => {
 		api.getInitialCards()
@@ -36,16 +24,16 @@ function Main(props) {
 				<div className="profile__overlay" onClick={ props.onEditAvatar }>
 					<img
 						className="profile__avatar"
-						src={ userAvatar }
-						alt={ userName } />
+						src={ user.avatar }
+						alt={ user.name } />
 				</div>
 				<div className="profile__info">
-					<h1 className="profile__name overflow-hidden">{ userName }</h1>
+					<h1 className="profile__name overflow-hidden">{ user.name}</h1>
 					<button
 						className="profile__edit-btn"
 						type="button"
 						onClick={ props.onEditProfile }></button>
-					<p className="profile__activity overflow-hidden">{ userDescription }</p>
+					<p className="profile__activity overflow-hidden">{ user.about }</p>
 				</div>
 				<button
 					className="profile__add-btn"
